ba39bf316ee3b57758349cfc3f9425b6da1702d9
Geoffrey Chen <chyj1993@gmail.com>
2021-04-01 06:33:04 +0200

    Add progress bar to the non-animated version

--- nyan cat progress bar but nyan cat doesn't move.js
+++ nyan cat progress bar but nyan cat doesn't move.js
@@ -1,9 +1,10 @@
 // ==UserScript==
-// @version         1
-// @name            nyan cat progress bar but nyan cat doesn't move
+// @version         2.1
+// @name            nyan cat progress bar
 // @description     nyannn
 // @compatible      firefox
 // @compatible      opera
+// @compatible      edge
 // @match           https://www.youtube.com/*
 // @exclude         *://www.youtube.com/tv*
 // @exclude         *://www.youtube.com/embed/*
@@ -14,7 +15,8 @@
 // @noframes
 // ==/UserScript==
 
-(function() {var css = [`.ytp-play-progress {
+(function() {var css = [`
+.ytp-play-progress {
     padding: 2.5px 1px;
     top: -2px;
     /* pretty rainbow */
@@ -26,24 +28,41 @@
 	    color-stop(67%, #09f),
 	    color-stop(83%, #63f));
 }
-
 .ytp-scrubber-container {
-    /* nyanimated gif */
-    background: url('https://raw.githubusercontent.com/tntmod54321/Youtube-nyan-cat-progress-bar-userscript/master/nyantianimated.png') no-repeat;
+    /* nyanimated png*/
+		background: url('https://raw.githubusercontent.com/tntmod54321/Youtube-nyan-cat-progress-bar-userscript/master/nyantianimated.png') no-repeat;
     background-size: 36px;
     width: 40px;
     height: 24px;
     margin-top: -4px;
 }
-
 .ytp-scrubber-container:hover {
-    background-size:40px;
+    /* a bit more visable hover*/
+    background-size:45px;
+    width: 45px;
+    height: 28px;
     margin-top:-6px;
     margin-left:-2px;
 }
-
-.ytp-scrubber-button {
-    display: none;
+.ytp-scrubber-button{
+    display: none
+}
+.ytp-progress-bar {
+    /*a bit more visable progress bar*/
+    height: 10px;
+}
+.ytp-load-progress {
+    /*nyancat background gif*/
+    background: url('https://raw.githubusercontent.com/CYuZeric/Youtube-nyan-cat-progress-bar-userscript/master/nyancatbackground.gif') !important;
+}
+.ytp-progress-bar-container:hover .ytp-scrubber-button .ytp-progress-bar-container:hover .ytp-load-progress{
+    image-rendering: pixelated;
+}
+.html5-progress-bar-container, .ytp-progress-bar-container {
+	height: 12px !important;
+}
+.html5-progress-bar, .ytp-progress-bar {
+   margin-top: 12px !important;
 }`
 ].join("\n");
 if (typeof GM_addStyle != 'undefined') {
