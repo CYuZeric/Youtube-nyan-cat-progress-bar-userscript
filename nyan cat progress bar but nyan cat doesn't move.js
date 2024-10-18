0e51cff4270a587d6e8e20d97bd6fa43fd566638
Geoffrey <chyj1993@gmail.com>
2024-10-18 12:13:05 +0200

    Update nyan cat progress bar but nyan cat doesn't move.js
    update the png file

--- nyan cat progress bar but nyan cat doesn't move.js
+++ nyan cat progress bar but nyan cat doesn't move.js
@@ -1,10 +1,7 @@
 // ==UserScript==
-// @version         2.1
 // @name            nyan cat progress bar
+// @version         2.3
 // @description     nyannn
-// @compatible      firefox
-// @compatible      opera
-// @compatible      edge
 // @match           https://www.youtube.com/*
 // @exclude         *://www.youtube.com/tv*
 // @exclude         *://www.youtube.com/embed/*
@@ -15,69 +12,86 @@
 // @noframes
 // ==/UserScript==
 
-(function() {var css = [`
-.ytp-play-progress {
-    padding: 2.5px 1px;
-    top: -2px;
-    /* pretty rainbow */
-    background: -webkit-gradient(linear,  left top,  left bottom,
-	    color-stop(0.00, #f00),
-	    color-stop(17%, #f90),
-	    color-stop(33%, #ff0),
-	    color-stop(50%, #3f0),
-	    color-stop(67%, #09f),
-	    color-stop(83%, #63f));
-}
-.ytp-scrubber-container {
-    /* nyanimated png*/
-		background: url('https://raw.githubusercontent.com/tntmod54321/Youtube-nyan-cat-progress-bar-userscript/master/nyantianimated.png') no-repeat;
-    background-size: 36px;
-    width: 40px;
-    height: 24px;
-    margin-top: -4px;
-}
-.ytp-scrubber-container:hover {
-    /* a bit more visable hover*/
-    background-size:45px;
-    width: 45px;
-    height: 28px;
-    margin-top:-6px;
-    margin-left:-2px;
-}
-.ytp-scrubber-button{
-    display: none
-}
-.ytp-progress-bar {
-    /*a bit more visable progress bar*/
-    height: 10px;
-}
-.ytp-load-progress {
-    /*nyancat background gif*/
-    background: url('https://raw.githubusercontent.com/CYuZeric/Youtube-nyan-cat-progress-bar-userscript/master/nyancatbackground.gif') !important;
-}
-.ytp-progress-bar-container:hover .ytp-scrubber-button .ytp-progress-bar-container:hover .ytp-load-progress{
-    image-rendering: pixelated;
-}
-.html5-progress-bar-container, .ytp-progress-bar-container {
-	height: 12px !important;
-}
-.html5-progress-bar, .ytp-progress-bar {
-   margin-top: 12px !important;
-}`
-].join("\n");
-if (typeof GM_addStyle != 'undefined') {
- GM_addStyle(css);
- } else if (typeof PRO_addStyle != 'undefined') {
- PRO_addStyle(css);
- } else if (typeof addStyle != 'undefined') {
- addStyle(css);
- } else {
- var node = document.createElement('style');
- node.type = 'text/css';
- node.appendChild(document.createTextNode(css));
- var heads = document.getElementsByTagName('head');
- if (heads.length > 0) { heads[0].appendChild(node);
- } else {
- document.documentElement.appendChild(node);
- }
-}})();
+(function() {
+    const css = `
+        .ytp-play-progress {
+            padding: 2.5px 1px;
+            top: -2px;
+            /* pretty rainbow with modern syntax */
+            background: linear-gradient(
+                to bottom,
+                #f00 0%,
+                #f90 17%,
+                #ff0 33%,
+                #3f0 50%,
+                #09f 67%,
+                #63f 83%
+            ) !important;
+            opacity: 1 !important;
+            z-index: 1000 !important;
+        }
+        .ytp-scrubber-container {
+            background: url('https://raw.githubusercontent.com/CYuZeric/Youtube-nyan-cat-progress-bar-userscript/master/nyanimated.png') no-repeat;
+            background-size: 36px;
+            width: 40px;
+            height: 24px;
+            margin-top: -4px;
+        }
+        .ytp-scrubber-container:hover {
+            background-size: 45px;
+            width: 45px;
+            height: 28px;
+            margin-top: -6px;
+            margin-left: -2px;
+        }
+        .ytp-scrubber-button {
+            display: none;
+        }
+        .ytp-progress-bar {
+            height: 10px;
+        }
+        .ytp-load-progress {
+            background: url('https://raw.githubusercontent.com/CYuZeric/Youtube-nyan-cat-progress-bar-userscript/master/nyancatbackground.gif') !important;
+        }
+        .ytp-progress-bar-container:hover .ytp-scrubber-button .ytp-progress-bar-container:hover .ytp-load-progress {
+            image-rendering: pixelated;
+        }
+        .html5-progress-bar-container, .ytp-progress-bar-container {
+            height: 12px !important;
+        }
+        .html5-progress-bar, .ytp-progress-bar {
+            margin-top: 12px !important;
+        }
+    `;
+
+    if (typeof GM_addStyle != 'undefined') {
+        GM_addStyle(css);
+    } else if (typeof PRO_addStyle != 'undefined') {
+        PRO_addStyle(css);
+    } else if (typeof addStyle != 'undefined') {
+        addStyle(css);
+    } else {
+        var node = document.createElement('style');
+        node.type = 'text/css';
+        node.appendChild(document.createTextNode(css));
+        var heads = document.getElementsByTagName('head');
+        if (heads.length > 0) {
+            heads[0].appendChild(node);
+        } else {
+            document.documentElement.appendChild(node);
+        }
+    }
+
+    const observer = new MutationObserver(() => {
+        const progressBar = document.querySelector('.ytp-play-progress');
+        if (progressBar) {
+            // Stop observing after styles are applied
+            observer.disconnect();
+        }
+    });
+
+    observer.observe(document.documentElement, {
+        childList: true,
+        subtree: true
+    });
+})();
